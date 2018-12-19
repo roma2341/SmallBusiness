4bbe759daafb92eb382f5bdf349dc8b8f76c2f34
Zinchuk Roman <zigzag2341@gmail.com>
2018-12-19 23:56:05 +0200

    Basic registration form design

--- src/app/shared-material/shared-material.module.ts
+++ src/app/shared-material/shared-material.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SharedMaterialComponent } from './shared-material.component';
 import {MatCardModule, MatButtonModule, MatIconModule, MatInputModule, MatToolbarModule, MatProgressSpinnerModule} from '@angular/material';
 @NgModule({
   imports: [
