8f267f992feb33cee60a04f723f993e0a1586438
Roman zinchuk <zigzag2341@gmail.com>
2018-12-26 15:32:41 +0200

    Validation improved

--- src/app/pages/auth/registration/registration-page.component.ts
+++ src/app/pages/auth/registration/registration-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/security/models/user.model';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-registration-page',
@@ -10,6 +11,11 @@ export class RegistrationPageComponent implements OnInit {
   user:User;
   constructor() {
     this.user = new User();
+  }
+  onFormSubmit(f: NgForm){
+    if(f.valid){
+      //TODO
+    }
   }
 
   ngOnInit() {
